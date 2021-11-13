fad7a8cc6f58a15b215c0443ff1e101c0350cc3e
santosh <santoshlokare555@gmail.com>
2021-11-13 10:35:54 +0530

    initial commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -131,7 +131,21 @@ export class HomeComponent implements OnInit {
       requested:"requested today 2 PM",
     },
   ];
+  isMove:boolean;
   constructor() {}
 
   ngOnInit(): void {}
+  onLargeCardClick(direction){
+    this.isMove=true;
+    // let distance = listRef.current.getBoundingClientRect().x - 50;
+    // if (direction === "left" && slideNumber > 0) {
+    //   setSlideNumber(slideNumber - 1);
+    //   listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    // }
+    // if (direction === "right" && slideNumber < 5) {
+    //   setSlideNumber(slideNumber + 1);
+    //   listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    // }
+  }
+
 }
